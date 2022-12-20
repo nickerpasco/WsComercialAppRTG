90b24d5586565714907b67808052600445a35cc9
Nicker <pascon@royalsystems.net>
2022-12-20 11:52:30 -0500

    add

--- WsComercialApp/Models/Model_LetrasCabecera.cs
+++ WsComercialApp/Models/Model_LetrasCabecera.cs
@@ -14,6 +14,11 @@ namespace WsComercialApp.Models
 		public string LabelMes { get; set; }
 		public string LabelHora { get; set; }
 		public int IdPersona { get; set; }
+		public int ClienteDireccionSecuencia { get; set; }
+		public int DiasCanje { get; set; }
+		public int LetrasCantidadValorReal { get; set; }
+		public DateTime? FechaBase { get; set; }
+		public DateTime? FechaMaxima { get; set; }
 		public string NombreCompleto { get; set; }
 		public int OperacionCanjeNumero { get; set; }
 		public int? Vendedor { get; set; }
